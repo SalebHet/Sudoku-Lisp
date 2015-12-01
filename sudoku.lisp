3a63ce5c252393c6c63b1b7fca83b3fa320683b4
emeryk <mr.emeryk@gmail.com>
2015-12-01 17:08:14 +0100

    dotimes qui marche

--- sudoku.lisp
+++ sudoku.lisp
@@ -1,10 +1,10 @@
-(defun create_grid ()
+(defun create-grid ()
   (make-array '(9 9)))
 
 
 (defun affiche-grille (grid)
   (princ "   | A B C | D E F | G H I |" ) (terpri)
   (princ "**********************************")(terpri)
-   (dotimes i 1 (length grid)
+  (dotimes (i (car (array-dimensions grid)))
 	    (princ i))
 )
