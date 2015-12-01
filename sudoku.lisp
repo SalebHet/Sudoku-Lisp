a60236109c2fc5488cfd7497d5a5a760b7dffe78
SalebHet <clement.nerestan@gmail.com>
2015-12-01 16:10:13 +0100

    mise à jour de l'affichage

--- sudoku.lisp
+++ sudoku.lisp
@@ -3,5 +3,8 @@
 
 
 (defun affiche-grille (grid)
-(print grid)
+  (princ "   | A B C | D E F | G H I |" ) (terpri)
+  (princ "**********************************")(terpri)
+   (dotimes i 1 (length grid)
+	    (princ i))
 )
