240a2c1a238a5eaec245d9bf1b5f9eaee2240cce
emeryk <mr.emeryk@gmail.com>
2015-12-11 22:06:59 +0100

    suppression des fonctions en trop

--- Strategy.lisp
+++ Strategy.lisp
@@ -21,18 +21,13 @@
 (defparameter *s2gsolved* ())
 (defparameter *s2gpossibles* ())
 
-(defun create-grid ()
-  (make-array '(9 9)))
-
-(defun create-grid-full ()
-  (make-array '(9 9) :initial-element 9))
-
-(defun copy-grid (grid)
+(defun copy-grid (grid) ;Retourne la copie de la grille donnée en paramètre
   (let ((grid2 (make-array (array-dimensions grid))))
     (dotimes (i (array-dimension grid 0))
       (dotimes (j (array-dimension grid 1))
         (setf (aref grid2 i j) (aref grid i j))))
     (return-from copy-grid grid2)))        
+      
 
 (defun show-grid (grid grid2)
   (format t "   | A B C | D E F | G H I |~%" )
@@ -53,39 +48,7 @@
       (format t "|~%")))
   (format t "****************************~%"))
 
-(defun add-number (grid grid2 x y numb)
-  (if (valid-position grid2 x y numb)
-      (setf (aref grid y x) numb)))
 
-(defun valid-position (grid x y numb)
-  (if (/= (aref grid y x) 0)
-      (return-from valid-position NIL))
-  (dotimes (i 9) (if (= numb (aref grid y i))
-		     (return-from valid-position NIL)))
-  (dotimes (i 9) (if (= numb (aref grid i x))
-		     (return-from valid-position NIL)))
-  (dotimes (i 3)
-    (dotimes (j 3)
-      (if (= numb (aref grid (+ (* 3 (floor y 3)) i) (+ (* 3 (floor x 3)) j)))
-	  (return-from valid-position NIL))))
-  T)
-
-(defun game-over (grid)
-  (dotimes (i 9)
-    (dotimes (j 9)
-      (if (= 0 (aref grid j i))
-	  (return-from game-over NIL))))
-  (return-from game-over T))
-
-(defun sudoku (grid)
-  (let ((grid2 (copy-grid grid)))
-    (loop while (not (game-over grid2)) do
-       (show-grid grid2 grid)
-       (format t " Ajouter un nombre : Colonne Ligne Nombre ~%")
-       (if (not (add-number grid2 grid
-			    (cdr (assoc (read) '((A . 0) (B . 1) (C . 2) (D . 3) (E . 4) (F . 5) (G . 6) (H . 7) (I . 8))))
-			    (- (read) 1) (read)))
-	   (format t " Position non valide !~%")))))
 
 (defun init-standalone (grid)
   (setf *s2gsolved* (copy-grid grid))
@@ -101,25 +64,25 @@
         (progn
           (loop for i from 0 to 8 if (/= i x) do
             (if (check x y i y)
-              (return-from main-standalone "BLUBLUBLUBBBB1")))
+              (return-from main-standalone "A compléter")))
           (loop for j from 0 to 8 if (/= j y) do
 	    (if (check x y x j)
-              (return-from main-standalone "BLUBLUBLUBBBB2")))
+              (return-from main-standalone "A compléter")))
 	  (let ((l (list-block x y)))
 	    (loop for b in l do
               (if (not (equal (cons x y) l))
 	        (if (check x y (car b) (cdr b))
-                  (return-from main-standalone "BLUBLUBLUBBBB3")))))
+                  (return-from main-standalone "A compléter")))))
           (loop for i from 1 to 9 do (progn
             (if (only-in-line x y i)(progn
 	      (setf (aref *s2gsolved* y x) i)
-              (return-from main-standalone "BLUBLUBLUBBBB4")))
+              (return-from main-standalone "A compléter")))
             (if (only-in-column x y i)(progn
 	      (setf (aref *s2gsolved* y x) i)
-              (return-from main-standalone "BLUBLUBLUBBBB5")))
+              (return-from main-standalone "")))
             (if (only-in-block x y i)(progn
 	      (setf (aref *s2gsolved* y x) i)
-              (return-from main-standalone "BLUBLUBLUBBBB6"))))))))))
+              (return-from main-standalone "A compléter"))))))))))
 
 (defun check (x y i j);x,y is not resolved
   (if (not (zerop (aref *s2gsolved* j i)))
@@ -130,16 +93,10 @@
         (if (= (aref *s2gpossibles* x y 0) 1)
 	  (progn
             (setf (aref *s2gsolved* y x) (only-possibility *s2gpossibles* x y))
-            (return-from check "BLUBLUB"))))))
+            (return-from check "A compléter"))))))
   nil);should return
   ;(if equal teste si les meme -> supprimer possibilites lignes/colonnes/block
   
-;(defun list-block (x y l)
-;  (if (zerop (mod (+ 1 x) 3))
-;    (if (zerop (mod (+ 1 y) 3))
-;      (cons (cons x y) l)
-;      (cons (cons x y) (list-block (- x 2) (+ 1 y) l)))
-;    (cons (cons x y) (list-block (+ 1 x) y l)))) 
 
 (defun list-block-t (x y l)
   (if (zerop (mod (+ 1 x) 3))
@@ -151,11 +108,7 @@
 (defun list-block (x y)
   (list-block-t (* 3 (floor x 3)) (* 3 (floor y 3)) '()))
 
-;(defun possibilities-number (p x y)
-;  (let ((c 0))
-;    (dotimes (i 9)
-;      (if (eq (aref p x y i) T)
-;        (setf c (+ c 1))))));;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;RETURN C
+
 
 (defun only-possibility (p x y)
   (loop for i from 1 to 9 do
@@ -190,18 +143,7 @@
             (if (aref *s2gpossibles* (car b) (cdr b) p)
               (return-from only-in-block nil)))))))
   T)
-(defun only-in-blockw (x y p)
-  (let ((l (list-block x y)))
-    (loop for b in l do
-      (if (not (equal (cons x y) b))
-        (if (= (aref *s2gsolved* (cdr b) (car b)) p)(progn
-          (format t "NiQUER LA GAME1 ~%")
-            (return-from only-in-blockw nil))
-          (if (zerop (aref *s2gsolved* (cdr b) (car b)))
-            (if (aref *s2gpossibles* (car b) (cdr b) p)(progn
-              (format t "NiQUER LA GAME2 ~a ~a ~%"(car b) (cdr b) )
-              (return-from only-in-blockw nil))))))))
-  T)
+
 (defun resolve(grid)
   (init-standalone grid)
   ;(loop while (not (game-over grid)) do (progn
